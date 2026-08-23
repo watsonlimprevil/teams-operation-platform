b0bba8bd5b017b8ad29f8c2aae2ceff064bbd8d1
watson limprevil <limprevil35@gmail.com>
2026-08-23 17:41:09 -0400

    added a auth route

---
+++ frontend/src/context/AuthContext.jsx
@@ -0,0 +1,34 @@
+import { createContext , useState , useEffect } from "react";
+
+export const AuthContext = createContext();
+
+export default function AuthProvider({children}){
+    const [token , setToken] = useState(null);
+    const [isAuthenticated , setIsAuthenticated] = useState(false);
+
+    useEffect(()=>{
+        const storedToken = localStorage.getItem('token');
+        if(storedToken){
+            setToken(storedToken);
+            setIsAuthenticated(true);
+        }
+    }, []);
+
+    const login = (newToken) =>{
+        localStorage.setItem('token' , newToken);
+        setToken(newToken);
+        setIsAuthenticated(true);
+    };
+
+    const logout = () =>{
+        localStorage.removeItem('token');
+        setToken(null);
+        setIsAuthenticated(false)
+    };
+
+    return(
+        <AuthContext.Provider value={{token , isAuthenticated , login , logout}}>
+            {children}
+        </AuthContext.Provider>
+    )
+}
