ac8a4e91e7d55687044e5f993398418ad78e578f
watson limprevil <limprevil35@gmail.com>
2026-08-30 20:19:55 -0400

    altered the api to add credentials trur

--- frontend/src/api/axios.js
+++ frontend/src/api/axios.js
@@ -1,15 +1,7 @@
 import axios from 'axios';
 
 const api = axios.create({
-    baseURL: 'http://localhost:3001',
-});
-
-api.interceptors.request.use((config) => {
-    const token = localStorage.getItem('token');
-    if(token){
-        config.headers.Authorization = `Bearer ${token}`
-    }
-    return config;
-});
-
+    baseURL : 'https://localhost:3001',
+    withCredentials:true,
+})
 export default api;
